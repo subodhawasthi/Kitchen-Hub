a8b308ce2e3d8add770f464110ff1b0a4035572b
subodhawasthi <awasthisubodhoni@gmail.com>
2024-08-08 15:29:24 +0530

    class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,12 @@
+import User from "./User";
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
         <div>
             <h1>About</h1>
+            <User name={"Subodh (function)"}/>
+            <UserClass name={"Subodh (Class)"} location={"Mathura"} />
         </div>
     );
 };
