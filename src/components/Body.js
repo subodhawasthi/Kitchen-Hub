6e340f3c9a7573df5168678cdfb878e1771236d9
Subodh Kant Awasthi <subodhawasthi@Subodhs-MacBook-Air.local>
2024-12-04 13:17:10 +0530

    Update colour

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,77 +1,104 @@
-import RestaurantCard, {withPromotedLabel} from "./RestaurantCard";
+import RestaurantCard, { withPromotedLabel } from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
-    const [listOfRestaurants, setListOfRestaurants] = useState([]);
-    const [filteredRestaurant, setFilteredRestaurants] = useState([]);
-    const [searchText, setSearchText] = useState("");
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
-    const PromotedRestaurantCard = withPromotedLabel(RestaurantCard);
+  const PromotedRestaurantCard = withPromotedLabel(RestaurantCard);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-        const data = await fetch(
-            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-        );
-
-        const json = await data.json();
-
-        console.log(json);
-
-        setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-        setFilteredRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    };
-
-    const onlineStatus = useOnlineStatus();
-
-    if(onlineStatus === false)
-        return (<h1>Looks like you are offline!! Please check your internet connection!</h1>);
-
-    if (listOfRestaurants.length === 0) {
-        return <Shimmer />
-    }
-
-    return (
-        <div className="body">
-            <div className="filter mr-20 flex justify-between">
-                <div className="m-4 p-4">
-                    <input type="text" className="border border-solid border-black" value={searchText} onChange={(e) => {
-                        setSearchText(e.target.value);
-                    }}  
-                    />
-                    <button className="px-4 py-2 bg-yellow-200 m-4 rounded-xl" onClick={() => {
-
-                        const filteredRestaurant = listOfRestaurants.filter((res) => 
-                            res.info.name.toLowerCase().includes(searchText.toLowerCase())
-                    );
-                    setFilteredRestaurants(filteredRestaurant);
-                    }}>Search</button>
-                </div>
-                <div className="m-4 p-4 flex items-center">
-                <button className="px-4 py-2 bg-red-300 rounded-lg" onClick={() => {
-                    const filteredList = listOfRestaurants.filter(
-                        (res) => res.info.avgRating > 4
-                    );
-                    setFilteredRestaurants(filteredList);
-                }}>
-                    Top Rated
-                </button>
-                </div>
-            </div>
-            <div className="flex flex-wrap space-x-2">
-                {filteredRestaurant.map((restaurants) => (
-                    <Link key={restaurants.info.id} to={"/restaurants/"+ restaurants.info.id}>
-                        {restaurants.info.isOpen ? (<PromotedRestaurantCard resData={restaurants} />) : (<RestaurantCard resData={restaurants}/>)}</Link>
-                )) }
-            </div>
-        </div>
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5355161&lng=77.3910265&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
+
+    const json = await data.json();
+
+    console.log(json);
+
+    setListOfRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredRestaurants(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
+
+  const onlineStatus = useOnlineStatus();
+
+  if (onlineStatus === false)
+    return (
+      <h1>
+        Looks like you are offline!! Please check your internet connection!
+      </h1>
+    );
+
+  if (listOfRestaurants.length === 0) {
+    return <Shimmer />;
+  }
+
+  return (
+    <div className="bg-slate-600">
+      <div className="filter mr-20 flex justify-between">
+        <div className="m-4 p-4">
+          <input
+            type="text"
+            className="border border-solid border-black"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="px-4 py-2 bg-yellow-200 m-4 rounded-xl"
+            onClick={() => {
+              const filteredRestaurant = listOfRestaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurants(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <div className="m-4 p-4 flex items-center">
+          <button
+            className="px-4 py-2 bg-red-300 rounded-lg"
+            onClick={() => {
+              const filteredList = listOfRestaurants.filter(
+                (res) => res.info.avgRating > 4
+              );
+              setFilteredRestaurants(filteredList);
+            }}
+          >
+            Top Rated
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-between px-20 space-x-2">
+        {filteredRestaurant.map((restaurants) => (
+          <Link
+            key={restaurants.info.id}
+            to={"/restaurants/" + restaurants.info.id}
+          >
+            {restaurants.info.isOpen ? (
+              <PromotedRestaurantCard resData={restaurants} />
+            ) : (
+              <RestaurantCard resData={restaurants} />
+            )}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,35 +1,37 @@
 import { CDN_URL } from "../utils/constants";
 
 const RestaurantCard = (props) => {
-    const { resData } = props;
-    const { cloudinaryImageId, name, avgRating, cuisines, costForTwo, } = resData?.info;
+  const { resData } = props;
+  const { cloudinaryImageId, name, avgRating, cuisines, costForTwo } =
+    resData?.info;
 
-    return (
-        <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-50 hover:bg-yellow-200">
-            <img 
-                className="rounded-lg"
-                alt="res-logo"
-                src={ CDN_URL + cloudinaryImageId } />
-            <h3 className="font-bold py-2 text-lg">{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
-            <h4>{avgRating} stars</h4>
-            <h4>{costForTwo}</h4>
-            <h4>{resData.info.sla.deliveryTime} minutes</h4>
-        </div>
-    );
+  return (
+    <div className="m-4 p-4 w-[250px] h-[510px] rounded-lg bg-gray-50 hover:bg-yellow-200">
+      <img
+        className="rounded-lg"
+        alt="res-logo"
+        src={CDN_URL + cloudinaryImageId}
+      />
+      <h3 className="font-bold py-2 text-lg">{name}</h3>
+      <h4>{cuisines.join(", ")}</h4>
+      <h4>{avgRating} stars</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{resData.info.sla.deliveryTime} minutes</h4>
+    </div>
+  );
 };
 
-export  const withPromotedLabel = (RestaurantCard) => {
-    return (props) => {
-        return (
-            <div>
-                <label className="absolute bg-black text-white m-2 p-2 rounded-lg">
-                    Open
-                </label>
-                <RestaurantCard {...props} />
-            </div>
-        )
-    }
-}
+export const withPromotedLabel = (RestaurantCard) => {
+  return (props) => {
+    return (
+      <div>
+        <label className="absolute bg-black text-white m-2 p-2 rounded-lg">
+          Open
+        </label>
+        <RestaurantCard {...props} />
+      </div>
+    );
+  };
+};
 
 export default RestaurantCard;
